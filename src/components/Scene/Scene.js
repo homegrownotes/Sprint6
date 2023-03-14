a86afcda053871f4173bd97cd31ce0ed49562fa1
homegrownotes <homegrownotes@gmail.com>
2023-03-14 09:52:10 +0100

    Exercici 1

---
+++ src/components/Scene/Scene.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Scene() {
+	return (
+		<div className="container">
+			<p className="text">El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+		</div>
+	);
+}
+
+export default Scene;
